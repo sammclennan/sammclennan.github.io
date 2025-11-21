f3a6b875cf2ec8d87f00515b6bfe727a5648fbcb
sammclennan <samuelmclnz@gmail.com>
2025-11-21 13:42:28 +0900

    Initial commit

---
+++ language_data.js
@@ -0,0 +1,162 @@
+export const LANGUAGE_DATA = {
+    en: {
+        navBar: {
+            home: "Home",
+            about: "About",
+            projects: "Projects",
+            contact: "Contact",
+            github: "GitHub",
+            languageButton: "English",
+        },
+        homepage: {
+            heading: "Sam\n<wbr>McLennan",
+            subheading: "Software Developer | Educator | Language Enthusiast",
+        },
+        aboutMe: {
+            heading: "About Me",
+            p1: "I'm a self-taught programmer with a formal education in finance and hands-on experience across several fields, including electrical engineering and teaching.",
+            p2: "I first became interested in programming in 2024 when I discovered how writing a simple Python script could automate the process of creating flashcards for studying Japanese, saving me hours of repetitive copy-and-paste work.",
+            p3:"I was born in New Zealand and moved to Kagoshima, Japan in 2024, where I now live with my wife and our three-month-old son.",
+        },
+        mySkills: {
+            heading: "My Skills",
+            subheading: "I have experience solving problems in the following languages",
+        },
+        myProjects: {
+            heading: "Projects",
+            subheading: "Below are some of the projects I've built so far",
+            visitSiteButton: "Visit Site",
+            viewCodeButton: "View Code",
+            englishVocabChallenge: {
+                heading: "English Vocab Challenge",
+                body: "An interactive, multimedia web application that helps Japanese learners of English practise their recognition and output skills. It features a modern, easy-to-use interface and offers a high degree of customization.",
+            },
+            englishPronunciationChallenge: {
+                heading: "English Pronunciation Challenge",
+                body: "Originally a branch of English Vocab Challenge, this grew into a project of its own. It leverages the Web Speech API to allow users to improve their English pronunciation through an exciting time-based challenge.",
+            },
+            countdownTimer: {
+                heading: "Countdown Timer",
+                body: "A minimalistic, modern countdown timer that employs various audio and visual effects to clearly convey the passage of time.",
+            },
+            stopwatch: {
+                heading: "Stopwatch",
+                body: "An application that captures the essence of an analog stopwatch in appearance and functionality through code.",
+            },
+            cssFireworksDisplay: {
+                heading: "CSS Fireworks Display",
+                body: "An exploration of the possibilities of CSS to create dynamic animations and visual effects.",
+            },
+        },
+        otherWork: {
+            heading: "Other Work",
+            subheading: "A selection of my other code repositories",
+            cs50: {
+                heading: "CS50",
+                body: "My solutions to the problems from Harvard University's CS50: Introduction to Computer Science Course.",
+            },
+            pythonScriptsForStudyingJapanese: {
+                heading: "Python scripts for studying Japanese",
+                body: "Various Python scripts for extracting, formatting and organizing Japanese language data for study.",
+            },
+            bigBookOfSmallPythonProjects: {
+                heading: "The Big Book of Small Python Projects",
+                body: 'My own solutions to the 81 problems from the book "The Big Book of Small Python Projects" by Al Sweigart.',
+            }
+        },
+        contactMe: {
+            heading: "Contact Me",
+            subheading: "Let's talk about how I can contribute",
+            name: "Name",
+            namePlaceholder: "Name",
+            email: "Email",
+            emailPlaceholder: "Email",
+            message: "Message",
+            messagePlaceholder: "Enter your message",
+            sendButton: "Send",
+        },
+        footer: {
+            copyrightText: "© Sam McLennan 2025",
+        }
+    },
+    jp: {
+        navBar: {
+            languageButton: "日本語",
+            home: "ホーム",
+            about: "自己紹介",
+            projects: "プロジェクト",
+            contact: "お問い合わせ",
+            github: "GitHub"
+        },
+        homepage: {
+            heading: "サム・<wbr>マクレーネン",
+            subheading: "ソフトウェア開発者　｜　教育者　｜　言語好き",
+        },
+        aboutMe: {
+            heading: "自己紹介",
+            p1: "私は独学でプログラミングを学び、金融の正式な教育を受けつつ、電気工学や教育などさまざまな分野での実務経験もあります。",
+            p2: "プログラミングに初めて興味を持ったのは2024年のとこでした。簡単なPythonスクリプトを書くだけで、日本語学習用の単語カード作成を自動化でき、何時間もかかる単調なコピー&ペースト作業を省けるのに気づいたのです。",
+            p3:"私はニュージーランド出身で、2024に鹿児島県に引っ越しました。現在は鹿児島で妻と3ヶ月の息子と一緒に暮らしています。",
+        },
+        mySkills: {
+            heading: "スキル",
+            subheading: "以下の言語で開発経験があります",
+        },
+        myProjects: {
+            heading: "プロジェクト",
+            subheading: "これまで作成したプロジェクトの一部を紹介します。",
+            visitSiteButton: "サイトを見る",
+            viewCodeButton: "コードを見る",
+            englishVocabChallenge: {
+                heading: "英単語チャレンジ",
+                body: "日本人英語学習者が認識力とアウトプットを鍛えるための、インタラクティブなマルチメディアWebアプリです。モダンで使いやすいインタフェースと高いカスタマイズ性を備えています。",
+            },
+            englishPronunciationChallenge: {
+                heading: "英語発音チャレンジ",
+                body: "もともとは英単語チャレンジの派生プロジェクトでしたが、独立したプロジェクトとして発展しました。Web Speech APIを活用し、ユーザーは時間制のチャレンジを通して英語の発音を上達させることができます。",
+            },
+            countdownTimer: {
+                heading: "カウントダウンタイマー",
+                body: "音と視覚効果を用いて時間の経過をわかりやすく示す、ミニマルでモダンなカウントダウンタイマーです。",
+            },
+            stopwatch: {
+                heading: "ストップウォッチ",
+                body: "アナログストップウォッチの外観と機能をコードによって再現したアプリケーションです。",
+            },
+            cssFireworksDisplay: {
+                heading: "CSS花火大会",
+                body: "CSSを使って動的なアニメーションや視覚効果を作り出す可能性を探求した作品です。",
+            },
+        },
+        otherWork: {
+            heading: "その他のプロジェクト",
+            subheading: "その他のコードリポジトリを紹介します",
+            cs50: {
+                heading: "CS50",
+                body: "ハーバード大学のCS50: コンピュータサイエンスの入門の課題に対する私の解答です。",
+            },
+            pythonScriptsForStudyingJapanese: {
+                heading: "日本語勉強のためのPythonスクリプト",
+                body: "日本語データの抽出と整形を行うための小さなPythonスクリプト集です。",
+            },
+            bigBookOfSmallPythonProjects: {
+                heading: "The Big Book of Small Python Projects",
+                body: 'アル・スウェイガート著「The Big Book of Small Python Projects」の81の問題に対する私の実装です。',
+            },
+        },
+        contactMe: {
+            heading: "お問い合わせ",
+            subheading: "どのようにお力になるか、お話ししましょう",
+            name: "お名前",
+            namePlaceholder: "お名前",
+            email: "メールアドレス",
+            emailPlaceholder: "メールアドレス",
+            message: "メッセージ",
+            messagePlaceholder: "メッセージを入力してください",
+            sendButton: "送信",
+        },
+        footer: {
+            copyrightText: "© サム・マクレーネン 2025",
+        }
+    },
+}
